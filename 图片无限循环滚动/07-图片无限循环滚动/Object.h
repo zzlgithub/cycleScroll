57765562f3d1f68640c9dd99b80ba6542d497fe4
coding_long <947502908@qq.com>
2016-08-31 20:15:51 +0800

    添加文件

---
+++ 图片无限循环滚动/07-图片无限循环滚动/Object.h
@@ -0,0 +1,13 @@
+//
+//  Object.h
+//  07-图片无限循环滚动
+//
+//  Created by zhangzhenglong on 16/8/31.
+//  Copyright © 2016年 liuda065. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Object : NSObject
+
+@end
---
+++ 图片无限循环滚动/07-图片无限循环滚动/Object.m
@@ -0,0 +1,13 @@
+//
+//  Object.m
+//  07-图片无限循环滚动
+//
+//  Created by zhangzhenglong on 16/8/31.
+//  Copyright © 2016年 liuda065. All rights reserved.
+//
+
+#import "Object.h"
+
+@implementation Object
+
+@end
